205fb7106c8e0bb23b002800c7b25e40a8f11daa
Umuthan Uyan <mail@umuthan.com>
2020-01-12 00:59:59 +0300

    TASK 1 completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+class App extends Component {
+
+  render(){
+
+    return (
+
+      <Router>
+        <header>
+          <nav>
+            
+          </nav>
+        </header>
+
+        <section>
+          <Switch>
+
+          </Switch>
+        </section>
+    </Router>
+
+    );
+
+  }
+
 }
 
 export default App;
